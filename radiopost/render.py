c5e241d22e598aea0625278f30e828c420faed0e
He Can Jog <erik@hecanjog.com>
2019-12-14 02:17:55 +0000

    Updates for radio.af streaming

--- radiopost/render.py
+++ radiopost/render.py
@@ -4,31 +4,37 @@ from pathlib import Path
 from pippi import dsp
 import sys
 import time
+from mutagen.flac import FLAC
+import subprocess
+import hashlib
+import shutil
+
+def hashit(f):
+    sha1 = hashlib.sha1()
+    with open(f, 'rb') as b:
+        while True:
+            d = b.read(2**16)
+            if not d:
+                break
+            sha1.update(d)
+    return str(sha1.hexdigest())
+
 
 if __name__ == '__main__':
     from . import synth, segments, waves
 
-    seed = None
-
-    try:
-        flacs = sys.argv[1]
-        name = sys.argv[2]
-        if len(sys.argv) > 3:
-            seed = int(sys.argv[3])
-    except IndexError:
-        print('Usage: python -m radiopost.render <path-to-flacs> <name> <seed>')
+    FLACPATH = '/srv/radio/phonography/recordings'
+    name = 'r'
+    seed = int(time.time())
 
     (Path('renders') / Path(name)).mkdir(exist_ok=True)
     (Path('renders') / Path(name) / Path('waves')).mkdir(exist_ok=True)
     (Path('renders') / Path(name) / Path('stems')).mkdir(exist_ok=True)
     (Path('renders') / Path(name) / Path('graphs')).mkdir(exist_ok=True)
 
-    if seed is None:
-        seed = int(time.time())
-    dsp.seed(int(seed))
+    dsp.seed(seed)
 
-    options = [ str(p) for p in Path(sys.argv[1]).glob('*.flac') ]
-    options += [ str(p) for p in Path(sys.argv[1]).glob('*.wav') ]
+    options = [ str(p) for p in Path(FLACPATH).glob('*.flac') ]
 
     choices = []
     MLEN = dsp.rand(60 * 3, 60 * 20)
@@ -58,6 +64,31 @@ if __name__ == '__main__':
 
     print('combine')
     waves.combinewaves(name, seed)
+
+    final = Path('renders/%s/%s-combined.wav' % (name, seed))
+    catalognumber = 'phafg-%s' % hashit(final)
+    dest = Path('/srv/radio/phonography/generated/hcj01/%s.flac' % catalognumber)
+
+    r = subprocess.run(['sox', '-S', final, dest])
+    if r.returncode != 0:
+        raise Exception('Could not convert to flac: %s' % dest)
+
+    flac = FLAC(dest)
+
+    credits = []
+    for o in options:
+        credits += [FLAC(o)['title']]
+
+    flac['title'] = ', '.join(credits)
+    flac['catalognumber'] = catalognumber
+    flac.save()
+
+    Path('renders/%s' % name).unlink()
+
+    cover = Path('/srv/www/phonography.radio.af/covers/default.jpg')
+    dcover = Path('/srv/www/phonography.radio.af/covers/%s.jpg' % catalognumber)
+
+    shutil.copy(cover, dcover)
 
     print('DONE!')
 
